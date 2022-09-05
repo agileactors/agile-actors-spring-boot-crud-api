2a9b6667cea285649f4aa4a481fd9cd5dadd368f
Alexis Panousis <apanousis@gmail.com>
2022-09-05 17:02:57 +0300

    Fixed missing type declaration at abstract layer

--- src/main/java/com/agileactors/crud/data/jpa/repository/AbstractRepository.java
+++ src/main/java/com/agileactors/crud/data/jpa/repository/AbstractRepository.java
@@ -1,9 +1,11 @@
 package com.agileactors.crud.data.jpa.repository;
 
+import com.agileactors.crud.domain.AbstractPersistable;
+import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface AbstractRepository<T, I> extends JpaRepository<T, I> {
+public interface AbstractRepository<T extends AbstractPersistable<I>, I extends Serializable> extends JpaRepository<T, I> {
 
 }
